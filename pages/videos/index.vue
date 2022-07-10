48b45a9163173a1bca429ccdef1acdac7f3d5f8a
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-07-10 05:37:31 +0200

    Revert "Feature/videos"

--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -1,33 +1,9 @@
 <template>
-  <section>
-    <uiAddButton text="Add Video" @click="goTo()" />
-
-    <uiEmpty
-      v-if="!videos.length"
-      imgSrc="videos/no-videos.png"
-      caption="No videos have been added yet."
-    />
-
-    <videosVideoList :videos="videos" />
-  </section>
+  <div class="text-center">Videos</div>
 </template>
 
 <script>
-export default {
-  async fetch({ store }) {
-    await store.dispatch("videos/fetchVideos");
-  },
-  methods: {
-    goTo() {
-      this.$router.push("/videos/add");
-    },
-  },
-  computed: {
-    videos() {
-      return this.$store.state.videos.videos;
-    },
-  },
-};
+export default {};
 </script>
 
 <style></style>
