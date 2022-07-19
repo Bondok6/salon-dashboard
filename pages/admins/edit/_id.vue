e44d8d5d81da538c205da40bbb73ecd19e262e0e
Bondok6 <kirolousbondok6@gmail.com>
2022-07-19 21:16:54 +0200

    Finish Admins Section

---
+++ pages/admins/edit/_id.vue
@@ -0,0 +1,146 @@
+<template>
+  <section class="main-form mb-5">
+    <h2 class="main-form__title">Update User</h2>
+
+    <el-form :rules="formRules" :model="form" ref="form" class="mt-5">
+      <div class="d-flex align-items-center gap-4 mb-5">
+        <el-upload
+          class="upload-demo"
+          list-type="picture-card"
+          action="#"
+          :on-change="handleChange"
+          :on-remove="handleRemove"
+          :class="{ hideUpload: !showUpload }"
+          :show-file-list="true"
+          :auto-upload="false"
+          :limit="1"
+          accept="image/*"
+        >
+          <i class="el-icon-upload"></i>
+        </el-upload>
+        <div class="img-caption">
+          <h5>Image Of User</h5>
+          <h6>Please Upload Image 160*160</h6>
+        </div>
+      </div>
+
+      <div class="d-flex flex-wrap gap-5 mb-4">
+        <el-form-item label="User Name" prop="userName" style="width: 500px">
+          <el-input v-model="form.userName"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Phone" prop="phone" style="width: 500px">
+          <el-input
+            type="number"
+            v-model="form.phone"
+            placeholder="(+973) 1555389225"
+          >
+          </el-input>
+        </el-form-item>
+      </div>
+
+      <div class="d-flex flex-wrap gap-5 mb-4">
+        <el-form-item prop="password" label="Password" style="width: 500px">
+          <el-input v-model="form.password" type="password" show-password />
+        </el-form-item>
+
+        <el-form-item label="Role" prop="role">
+          <el-select
+            v-model="form.role"
+            placeholder="Select Role"
+            style="width: 500px"
+          >
+            <el-option
+              v-for="role in roleOptions"
+              :key="role.value"
+              :label="role.label"
+              :value="role.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </div>
+    </el-form>
+
+    <button class="btn btn--pink btn--add" @click.prevent="updateUser()">
+      Update
+    </button>
+    <button class="btn btn--white btn--add" @click.prevent="goTo()">
+      Cancel
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    const data = await this.$store.dispatch(
+      "admin/fetchUser",
+      this.$route.params.id
+    );
+    this.form = data;
+    this.form.password = "";
+    this.form.image = [];
+  },
+  data() {
+    return {
+      form: {},
+      formRules: {
+        userName: [
+          { required: true, message: "Please input name of employee" },
+        ],
+        phone: [{ required: true, message: "Please input phone number" }],
+        password: [{ required: true, message: "Please input password" }],
+        role: [{ required: true, message: "Please select a role" }],
+      },
+      roleOptions: [
+        {
+          value: "ADMIN",
+          label: "Admin",
+        },
+        {
+          value: "EMPLOYEE",
+          label: "Employee",
+        },
+      ],
+      showUpload: true,
+    };
+  },
+  methods: {
+    toggleUpload() {
+      this.showUpload = !this.showUpload;
+    },
+    handleChange(file) {
+      this.form.image.push(file.raw);
+      this.toggleUpload();
+    },
+    handleRemove() {
+      this.form.image.pop();
+      this.toggleUpload();
+    },
+    updateUser() {
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          if (this.form.image.length > 0) {
+            delete this.form.profile;
+          }
+          const loading = this.$loading();
+          try {
+            console.log(this.form);
+            await this.$store.dispatch("admin/updateUser", this.form);
+            this.$message.success("User Updated Successfully");
+            this.$router.push("/admins");
+          } catch (error) {
+            this.$message.error("User Update Failed");
+          } finally {
+            loading.close();
+          }
+        }
+      });
+    },
+    goTo() {
+      this.$router.push("/admins");
+    },
+  },
+};
+</script>
--- pages/admins/index.vue
+++ pages/admins/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="text-center">Admins</div>
+  <section>
+    <uiAddButton text="Add User" @click="goTo()" />
+
+    <uiEmpty
+      v-if="!users.length"
+      imgSrc="admin/no-users.png"
+      caption="No users have been added yet."
+    />
+
+    <adminUsersList :tableData="users" />
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  async fetch({ store }) {
+    await store.dispatch("admin/fetchUsers");
+  },
+  methods: {
+    goTo() {
+      this.$router.push("/admins/add");
+    },
+  },
+  computed: {
+    users() {
+      return this.$store.state.admin.users;
+    },
+  },
+};
 </script>
-
-<style></style>
