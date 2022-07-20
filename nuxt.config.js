52af17aef8f6a3c3f62520e8d66b44d7ccb0cbeb
Bondok6 <kirolousbondok6@gmail.com>
2022-07-20 17:41:49 +0200

    Add reservations assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "spa-dashboard",
+    title: "Amany Dashboard",
     htmlAttrs: {
       lang: "en",
     },
