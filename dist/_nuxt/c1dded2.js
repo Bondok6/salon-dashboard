64442d436800ba9e442b8e1219f6aede3f767f47
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-08-04 14:50:33 +0200

    Delete dist folder

--- dist/_nuxt/c1dded2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{559:function(t,e,n){"use strict";n.r(e);var c={},l=n(44),component=Object(l.a)(c,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("div",{staticClass:"text-center"},[t._v("Notifications")])}),[],!1,null,null,null);e.default=component.exports}}]);
